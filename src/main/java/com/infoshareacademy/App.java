f65573f620dec813af317dc02fb49855d00193fc
Dariusz Czajkowski <czajk.dariusz@gmail.com>
2019-11-28 22:51:06 +0100

    Add loggers for command app

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,7 +1,13 @@
 package com.infoshareacademy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+
+    private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        stdout.info("Hello World!");
     }
 }
