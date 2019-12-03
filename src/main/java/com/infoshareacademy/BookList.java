05b42cc113c582f22bc0df41fe0bc9a77a3fb26b
lukaszwolek <lucas83122@gmail.com>
2019-12-03 14:50:45 +0100

    aaa

--- src/main/java/com/infoshareacademy/BookList.java
+++ src/main/java/com/infoshareacademy/BookList.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class BookList implements KeyListener {
+public class BookList {
 
     Menu menu = new Menu();
     private int record = 1;
@@ -34,28 +34,26 @@ public class BookList implements KeyListener {
         }
 
         for (Book book : books) {
-            System.out.println("1. "+book);
+            System.out.println(record+". "+book);
             list.add(book);
+            int i;
             if (record > recordsLimit || record >= books.size()) {
-                //TODO: oczekiwanie na akcje uzytkownika:
-                Scanner scanner = new Scanner (System.in);
-                String choice1 = scanner.next();
-                ArrayList<String> letters = new ArrayList<String>();
-                letters.add("q");
-                letters.add("m");
+
                 System.out.println("Type your choice: ");
                 System.out.println("number of book to print a book");
-                if (!(record >=books.size())) {
-                    System.out.println("n - next page");
-                    letters.add("n");
+                if (!(record >= books.size())) {
+                    System.out.println("any key - next page");
                 }
                 System.out.println("m - main menu");
                 System.out.println("q - close application");
 
+                Scanner scanner = new Scanner (System.in);
+                String choice1 = scanner.next();
+
                 try {
-                    int i = Integer.valueOf(choice1);
+                    i = Integer.valueOf(choice1);
+                    printBook(i);
                 }  catch (NumberFormatException e)  {
-                    if (letters.contains(choice1)) {
                         ClearScreen.clearScreen();
                         switch (choice1) {
                             case "q": {
@@ -65,39 +63,20 @@ public class BookList implements KeyListener {
                             case "m": {
                                 menu.mainMenu();
                                 break;
-                                //TODO: case 'n'
                             }
-
                         }
                     }
+
+
                 }
-
-
-                ClearScreen.clearScreen();
-
 
             }
         }
-    }
+
 
     private void printBook(int choice) {
         System.out.println(list.get(choice));
 
         }
 
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
 }
