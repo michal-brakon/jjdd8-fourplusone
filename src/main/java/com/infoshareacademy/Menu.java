803fbb2b47a1f2369b3e2f7be6864169c6bc8cf6
lukaszwolek <lucas83122@gmail.com>
2019-12-05 14:35:07 +0100

    Revert "delete unnecesary files"
    This reverts commit 300f4cb6a7897c8be771cd9f1dcc0fbdd39e01e1.

---
+++ src/main/java/com/infoshareacademy/Menu.java
@@ -0,0 +1,101 @@
+package com.infoshareacademy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.Scanner;
+
+
+public class Menu {
+    private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
+    private int choice = 0;
+    private Scanner scan = new Scanner(System.in);
+
+    private int getChoice(int choices) {
+
+        stdout.info("\nType your choice: ");
+        String Choice = scan.nextLine();
+        //int choice = 0;
+
+        try {
+            choice = Integer.parseInt(Choice);
+        } catch (NumberFormatException e) {
+            stdout.info("\nYou typed a letter! ");
+            getChoice(choices);
+        }
+        if (choice>choices || choice<0)  {
+            stdout.info("\nPlease choice correct number! ");
+            getChoice(choices);
+        }
+
+        return choice;
+    }
+
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+
+        menu.mainMenu();
+    }
+
+    private void mainMenu()  {
+
+        stdout.info("\n1. wypozycz");
+        stdout.info("\n2. oddaj");
+        stdout.info("\n3. lista ksiazek");
+        stdout.info("\n-----");
+        stdout.info("\n0. wyjscie");
+
+        switch (getChoice(3))  {
+
+            case 1: {
+                wypozyczMenu();
+                break;
+            }
+            case 2: {
+                oddajMenu();
+                break;
+            }
+            case 3:  {
+                listaKsiazekMenu();
+                break;
+            }
+
+            case 0: exit();
+        }
+
+    }
+
+    private void exit() {
+        ClearScreen.clearScreen();
+        stdout.info("\nDo zobaczenia!");
+    }
+
+    private void listaKsiazekMenu() {
+        ClearScreen.clearScreen();
+        stdout.info("\n1. sortuj po autorze");
+        stdout.info("\n2. sortuj po gatunku");
+        stdout.info("\n3. sortuj po tytule");
+        stdout.info("\n4. main menu");
+        stdout.info("\n0. wyjscie");
+
+        switch (getChoice(4)) {
+            case 1: {}
+            case 2: {}
+            case 3: {}
+            case 4: {
+                mainMenu();
+                break;
+            }
+            case 0: exit();
+        }
+
+    }
+
+    private void oddajMenu()  {
+
+    }
+    private void wypozyczMenu()  {
+
+    }
+
+}
