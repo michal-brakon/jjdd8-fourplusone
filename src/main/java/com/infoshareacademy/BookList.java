aa0b8aba0896e66de94ec222817a671bccdea13b
lukaszwolek <lucas83122@gmail.com>
2019-12-05 14:33:53 +0100

    upgrade BookList

--- src/main/java/com/infoshareacademy/BookList.java
+++ src/main/java/com/infoshareacademy/BookList.java
@@ -1,9 +1,10 @@
 package com.infoshareacademy;
 
-import java.util.ArrayList;
-import java.util.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BookList {
     private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
@@ -11,23 +12,9 @@ public class BookList {
     ArrayList<Book> myList = new ArrayList<>();
     private int record;
     private int recordsLimit = 0;
-    int counter;
-    Menu menu = new Menu();
-    public static void main(String[] args) {
+    private int counter;
 
-//        BookList bookList = new BookList();
-//        //ArrayList<Book> myList = new ArrayList<Book>();
-//
-//        myList.add(new Book("dhahasds", "ysud", "tytul1", "sd", "dsas", "sas", "asd", false, "das"));
-//        myList.add(new Book("dsdfdssds", "ysud", "tytul2", "sd", "dsas", "sas", "asd", false, "das"));
-//        myList.add(new Book("dhahasds", "ysud", "tytul3", "sd", "dsas", "sas", "asd", false, "das"));
-//        myList.add(new Book("dhahasds", "ysvxcd", "tytul4", "sd", "dsas", "sas", "asd", false, "das"));
-//        myList.add(new Book("dhahasds", "ysud", "tytul5", "sd", "dsas", "sas", "asd", false, "das"));
-//        myList.add(new Book("dhahasds", "ysud", "tytul6", "sd", "dsas", "sas", "asd", false, "das"));
-//        myList.add(new Book("dhahasds", "ysud", "tytul7", "sd", "dsas", "sas", "asd", false, "das"));
-//        myList.add(new Book("dhahasds", "ysud", "tytul8", "sd", "dsas", "sas", "asd", false, "das"));
-//
-//        bookList.printBooks(myList);
+    public static void main(String[] args) {
 
     }
 
@@ -38,70 +25,77 @@ public class BookList {
         recordsLimit = 0;
 
         while (recordsLimit != 5 && recordsLimit != 10 && recordsLimit != 15) {
-            System.out.println("How many records on page? (5,10,15) ");
+            stdout.info("\nHow many records on page? (5,10,15) ");
             recordsLimit = menu.getChoice(15);
             if (recordsLimit != 5 && recordsLimit != 10 && recordsLimit != 15) System.out.println("Wrong number!");
         }
 
         for (Book book : books) {
 
-            if (record < books.size()+1)  System.out.println(record+ ". " + book);
+            if (record < books.size() + 1) System.out.println(record + ". " + book);
             counter++;
             record++;
 
-            if (counter > recordsLimit ) {
-                System.out.println("Type q to finish list, any to continue book list");
+            if (counter > recordsLimit) {
+                stdout.info("\nType q to finish list, any to continue book list");
                 Scanner scanner = new Scanner(System.in);
                 String choice = scanner.next();
-                if (choice.equals("q")) { record = books.size()+1;  }
+                if (choice.equals("q")) {
+                    record = books.size() + 1;
+                }
                 counter = 1;
+                ClearScreen.clearScreen();
             }
         }
-
-    menuBookList();
-
+        menuBookList();
     }
-    private void menuBookList ()  {
+
+    private void menuBookList() {
 
         counter = 1;
-        System.out.println("Type your choice: ");
-        System.out.println("c -       choose book");
-        System.out.println("m -       main menu");
-        System.out.println("q -       close application");
+        stdout.info("\nType your choice: ");
+        stdout.info("\nc -       choose book");
+        stdout.info("\nm -       main menu");
+        stdout.info("\nq -       close application");
 
         Scanner scanner = new Scanner(System.in);
         String choice1 = scanner.next();
 
         switch (choice1) {
             case "q": {
-                menu.exit();
+                exit();
                 break;
             }
             case "m": {
-                menu.mainMenu();
+
                 break;
             }
             case "c": {
-                System.out.println(myList.get(chooseBookToPrint()));
+                stdout.info(String.valueOf(myList.get(chooseBookToPrint())));
                 break;
             }
         }
 
         return;
     }
-    private int chooseBookToPrint () {
-        System.out.println("Wpisz numer ksiazki: ");
+
+    private int chooseBookToPrint() {
+        stdout.info("\nWpisz numer ksiazki: ");
         Scanner scanner = new Scanner(System.in);
         int bookChoice = 0;
         try {
-            bookChoice = scanner.nextInt() -1 ;
-        } catch (NumberFormatException e)  {
-            System.out.println("Type a number of book!");
+            bookChoice = scanner.nextInt() - 1;
+        } catch (NumberFormatException e) {
+            stdout.info("\nType a number of book!");
             chooseBookToPrint();
         }
 
         return bookChoice;
 
+    }
+    private void exit() {
+        ClearScreen.clearScreen();
+        stdout.info("\nDo zobaczenia!");
     }
 
 }
