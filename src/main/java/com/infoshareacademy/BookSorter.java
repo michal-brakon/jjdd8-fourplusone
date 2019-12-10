418a19b3307cf9e77103357531c9358ecd8c6cdc
michal-brakon <zielonydeveloper@o2.pl>
2019-12-10 16:55:28 +0100

    f commit

---
+++ src/main/java/com/infoshareacademy/BookSorter.java
@@ -0,0 +1,28 @@
+package com.infoshareacademy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BookSorter {
+    private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
