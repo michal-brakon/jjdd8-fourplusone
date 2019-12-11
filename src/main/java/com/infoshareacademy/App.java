55198ba247aba2faa750af2a373087189065d66e
Dariusz <czajk.dariusz@gmail.com>
2019-12-11 15:59:21 +0100

    erase imports

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -3,8 +3,6 @@ package com.infoshareacademy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-
 public class App {
 
     private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
