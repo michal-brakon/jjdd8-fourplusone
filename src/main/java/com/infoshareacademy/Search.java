da9322f605cd6ee10b4d2cf9d0c81cfbcb9358f6
lukaszwolek <lucas83122@gmail.com>
2019-12-11 16:58:00 +0100

    change Search.java

--- src/main/java/com/infoshareacademy/Search.java
+++ src/main/java/com/infoshareacademy/Search.java
@@ -1,11 +1,14 @@
 package com.infoshareacademy;
 
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 
 public class Search {
+
+    final static List<Book> BOOKS = BookRepository.getInstance().getBookRepository();
 
     //Search search = new Search();
 
@@ -23,37 +26,59 @@ public class Search {
 
     
 
-    private Set<String> getAuthors(List<Book> books) {
+    private String getAuthors(List<Book> books, String letters) {
 
-        Set<String> authors = new HashSet<>();
-        for (Book book : books) {
-            authors.add(book.getAuthor());
-        }
-        return authors;
-    }
-    private List<String> getAuthorsContainsLetters (String letters)  {
+        List<String> authors = new ArrayList<>();
+        String author = "";
 
-        //Set<String> collect = getAuthors(BookRepository.getInstance().getBookRepository());
 
-        List<String> authorsContainsLetters = getAuthors(BookRepository.getInstance().getBookRepository()).stream()
-                .filter(c -> c.contains(letters))
+
+
+        List<Book> sortedBooks = books.stream()
+                .filter(book -> book.getAuthor() != null && book.getAuthor().contains(letters))
                 .collect(Collectors.toList());
 
-        if (authorsContainsLetters.size() > 1) {
-            System.out.println(authorsContainsLetters);
-            System.out.println("Proszę uściślić wybór: ");
-            System.out.println(getAuthorsContainsLetters(getLetters()));
+        for (Book book : sortedBooks) {
+           if (!authors.contains(book.getAuthor())) {
+               authors.add(book.getAuthor());
+           }
         }
 
-        return authorsContainsLetters;
+        if (authors.size() > 1) {
+            System.out.println(authors);
+            System.out.println("Proszę uściślić wybór: ");
+            getLetters();
+        }  else if (authors.size() == 0) {
+            System.out.println("Nie znaleziono pasujacego autora! ");
+            getLetters();
+        }  else if (authors.size() == 1) {
+            author = authors.get(0);
+        }
+
+        return author;
+    }
+
+    private String chooseCorrectAuthor (Set<String> authors)  {
+
+        String author = "";
+
+//        if (authors.size() > 1) {
+//            System.out.println(authors);
+//            System.out.println("Proszę uściślić wybór: ");
+//            getLetters();
+//        }  else if (authors.size() == 0) {
+//            System.out.println("Nie znaleziono pasujacego autora! ");
+//            getLetters();
+//        }
+
+        return author;
     }
 
     public static void main (String[]args)  {
 
-
         Search search = new Search();
 
-        System.out.println(search.getAuthorsContainsLetters(search.getLetters()));
+        System.out.println(search.getAuthors(BOOKS, search.getLetters()));
 
         }
     }
