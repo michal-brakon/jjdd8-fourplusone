8622be8a653549a84c1a994d76e423daceb539d4
michal-brakon <zielonydeveloper@o2.pl>
2019-12-12 20:38:58 +0100

    working sorter, and one test added

--- src/main/java/com/infoshareacademy/BookSorter.java
+++ src/main/java/com/infoshareacademy/BookSorter.java
@@ -1,47 +1,42 @@
 package com.infoshareacademy;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class BookSorter {
     private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
 
-            List<Book> toSort = BookRepository.getInstance().getBookRepository();
-
-            List<Book> sorting (List toSort) {
-
-             List<Book> tralalala = (List<Book>) toSort.stream()
-                        .sorted(Comparator.reverseOrder())
-                        .collect(Collectors.toList());
-
-                return tralalala;
-            }
-
-
-    @Override
-    public String toString() {
-        return "BookSorter{" +
-                "toSort=" + toSort +
-                '}';
+    public List<Book> sortingByTitle(List<Book> books) {
+        return books.stream()
+                .sorted(Comparator.comparing(Book::getTitle))
+                .collect(Collectors.toList());
     }
 
-    public static void main(String[] args) {
-        BookSorter jaja = new BookSorter();
-
-        System.out.println(jaja.toSort);
+    public List<Book> sortingByKind(List<Book> books) {
+        return books.stream()
+                .sorted(Comparator.comparing(Book::getKind))
+                .collect(Collectors.toList());
     }
 
+    public List<Book> sortingByAuthor(List<Book> books) {
+        return books.stream()
+                .sorted(Comparator.comparing(Book::getAuthor))
+                .collect(Collectors.toList());
+    }
 
+    public List<Book> sortingByEpoch(List<Book> books) {
+        return books.stream()
+                .sorted(Comparator.comparing(Book::getEpoch))
+                .collect(Collectors.toList());
+    }
 
+    public List<Book> sortingByGenre(List<Book> books) {
+        return books.stream()
+                .sorted(Comparator.comparing(Book::getGenre))
+                .collect(Collectors.toList());
+    }
 }
 
 
