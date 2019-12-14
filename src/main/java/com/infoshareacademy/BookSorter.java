7c77941798c2015db05ba772ac3ab096b1b89d50
michal-brakon <zielonydeveloper@o2.pl>
2019-12-14 21:53:44 +0100

    path to config

--- src/main/java/com/infoshareacademy/BookSorter.java
+++ src/main/java/com/infoshareacademy/BookSorter.java
@@ -1,12 +1,36 @@
 package com.infoshareacademy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class BookSorter {
     private static final Logger stdout = LoggerFactory.getLogger("CONSOLE_OUT");
+
+
+    Properties properties = new Properties();
+
+    try(
+    FileReader fileReader = new FileReader("data/props.properties"))
+
+    {
+        properties.load(fileReader);
+    } catch(
+    IOException e)
+
+    {
+        e.printStackTrace();
+    }
+
+    public BookSorter() throws FileNotFoundException {
+    }
 
     public List<Book> sortingByTitle(List<Book> books) {
         return books.stream()
