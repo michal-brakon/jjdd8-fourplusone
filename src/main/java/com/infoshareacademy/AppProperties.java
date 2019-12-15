c5ede3e707f35c318d85d2c6e6c5b62d2a4d3d48
michal-brakon <zielonydeveloper@o2.pl>
2019-12-15 15:01:23 +0100

    reading from properties working

--- src/main/java/com/infoshareacademy/AppProperties.java
+++ src/main/java/com/infoshareacademy/AppProperties.java
@@ -1,73 +1,30 @@
 package com.infoshareacademy;
-import javax.xml.xpath.XPath;
+
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 public class AppProperties {
+    Properties properties = new Properties();
 
-
-//    String path = Thread.currentThread().getContextClassLoader().getResource("").getPath() + "config.properties";
-//
-//    AppProperties prop = new AppProperties();
-//    prop.load(new FileInputStream inputStream).;
-//
-//
-
-
-    @Override
-    public String toString() {
-        return "AppProperties{" +
-                "result='" + result + '\'' +
-                ", inputStream=" + inputStream +
-                '}';
-    }
-
-    private void load(InputStream inputStream) {
-    }
-
-    String result = "";
-    InputStream inputStream;
-
-    public String getPropValues() throws IOException {
-
+    public String getProp() {
         try {
-            Properties prop = new Properties();
-            String nazwaPliku = "config.properties";
-
-            inputStream = getClass().getClassLoader().getResourceAsStream(nazwaPliku);
-
-            if (inputStream != null) {
-                prop.load(inputStream);
-            } else {
-                throw new FileNotFoundException("Plik " + nazwaPliku + " nie znaleziony");
-            }
-        } catch (Exception e) {
-            System.out.println("Exception: " + e);
+            File file = new File("config.properties");
+            FileInputStream fileInput = new FileInputStream(file);
+            properties.load(fileInput);
+            fileInput.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Brak pliku!");
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-        return result;
+        return properties.getProperty("sortByTitle");
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         AppProperties n = new AppProperties();
-        System.out.println(n.getPropValues());
-
-
+        System.out.println(n.getProp());
     }
 }
-//
-//
-////
-////
-////    String configPath = Thread.currentThread().getContextClassLoader().getResource("").getPath() + "config.properties";
-////
-////
-////    Properties prop = new Properties();
-////        prop.load(new FileInputStream(configPath));
-////
-////
-////
-//
-//
