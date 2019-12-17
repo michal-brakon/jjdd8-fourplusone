ccf7b294ccded38c66edbe38d96f7b912fab2e59
lukaszwolek <lucas83122@gmail.com>
2019-12-17 16:59:21 +0100

    change FavouritesManager

--- src/main/java/com/infoshareacademy/FavouritesManager.java
+++ src/main/java/com/infoshareacademy/FavouritesManager.java
@@ -1,24 +1,73 @@
 package com.infoshareacademy;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class FavouritesManager {
 
-    public void addFavourites (Book book)  {
+    private static List<Book> BOOKS = new BookRepository().getInstance().getBookRepository();
 
-        String title = book.getTitle();
+    public void addFavourites (String title) throws FileNotFoundException {
+
+        List<String> lines = new ArrayList<>();
+        File file = new File("favourites.txt");
+        if (!isFileExist(file))  {
+            System.out.println("Blad odczytu pliku");
+            return;
+        }
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine())  {
+            lines.add(scanner.nextLine());
+        }
+        if (lines.contains(title)) {
+            System.out.println("Pozycja jest juz w ulubionych");
+            return;
+        }
+        if (lines.size() <= 2 && !lines.contains(title) {
+
+            PrintWriter printWriter = new PrintWriter("favourites.txt");
+            printWriter.println(title);
+            printWriter.close();
+        }   else if (lines.contains(title))  {
+            System.out.println("Pozycja jest juz w ulubionych");
+            return;
+        }   else  {
+            System.out.println("Lista ulubionych pelna");
+        }
+
     }
 
-    private boolean isFileExists (String filename) {
+    private void printFavourites() throws FileNotFoundException {
+        File file = new File("favourites.txt");
+        if (!isFileExist(file))  {
+            System.out.println("Blad odczytu pliku");
+            return;
+        }
+        Scanner scanner = new Scanner(file);
+        List<String> lines = new ArrayList<>();
+        while (scanner.hasNextLine())  {
+            lines.add(scanner.nextLine());
+        }
 
-        File f = new File(filename);
+
+    }
+
+    private boolean isFileExist(File f) {
+
         return f.exists() && f.isFile();
     }
 
-    private boolean isNotTooManyFavourites () {
-
-        File file = new File("favourites.txt");
-    }
-
-
 }
+
+
+
+
+
+
+
