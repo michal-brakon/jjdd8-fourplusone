8ca44c201941c65412d2af41c8a0c69fb9e08ba9
Dariusz Czajkowski <czajk.dariusz@gmail.com>
2020-01-17 07:54:44 +0100

    js and html changes

--- web/src/main/webapp/html/js/search.js
+++ web/src/main/webapp/html/js/search.js
@@ -1,19 +1,13 @@
-$("#fld_pozkoszt").keyup(function(){
+$("#sb-search").keyup(function(){
     if ($(this).val().lenght>=3){
         $.ajax({
             type: "POST",
-            url: "../manager/readPozkoszt.php",
+            url: "../search",
             data:'keyword='+$(this).val(),
-        beforeSend: function(){
-            $("#fld_pozkoszt").css("background","#FFF url(LoaderIcon.gif) no-repeat 165px");
-            $("#elementy").multiselect({
-                remoteUrl: "wn-ajax.php"
-            });
-        },
         success: function(data){
             $("#suggesstion-box2").show();
             $("#suggesstion-box2").html(data);
-            $("#fld_pozkoszt").css("background","#FFF");
+            $("#sb-search").css("background","#FFF");
             $("#elementy").multiselect( 'search', query );
         }
     });}})
