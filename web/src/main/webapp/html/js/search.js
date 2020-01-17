dfd2532da8c7a899e754e5f12d8ed94b40b2d12f
Dariusz Czajkowski <czajk.dariusz@gmail.com>
2020-01-17 08:11:02 +0100

    js search engine addded?

--- web/src/main/webapp/html/js/search.js
+++ web/src/main/webapp/html/js/search.js
@@ -1,13 +1,19 @@
-$("#sb-search").keyup(function(){
-    if ($(this).val().lenght>=3){
-        $.ajax({
-            type: "POST",
-            url: "../search",
-            data:'keyword='+$(this).val(),
-        success: function(data){
-            $("#suggesstion-box2").show();
-            $("#suggesstion-box2").html(data);
-            $("#sb-search").css("background","#FFF");
-            $("#elementy").multiselect( 'search', query );
+function $jQuery(document) {
+
+}
+
+$jQuery(document).ready(function($) {
+    $('#keyword').on('input', function() {
+        var searchKeyword = $(this).val();
+        if (searchKeyword.length >= 3) {
+            $.post('search.php', { keywords: searchKeyword }, function(data) {
+                $('ul#content').empty().addClass('active');
+                $.each(data, function() {
+                    $('ul#content').append('<li><a href="example.php?id=' + this.id + '">' + this.title + '</a></li>');
+                });
+            }, "json");
+        } else {
+            $('ul#content').empty().removeClass('active');
         }
-    });}})
+    });
+})
