4a0e617a2b3e52a60b1e942f7ee071707ee44161
lukaszwolek <lucas83122@gmail.com>
2020-01-18 21:06:47 +0100

    change download Api method

--- web/src/main/java/com/infoshareacademy/service/ApiDataInitializer.java
+++ web/src/main/java/com/infoshareacademy/service/ApiDataInitializer.java
@@ -1,60 +1,42 @@
 package com.infoshareacademy.service;
 
-import com.infoshareacademy.dao.*;
-import com.infoshareacademy.domain.api.*;
-import com.infoshareacademy.domain.entity.*;
-import com.infoshareacademy.mapper.*;
+import com.infoshareacademy.dto.BookDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.PostConstruct;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-//@Startup
-@Singleton
+@Stateless
 public class ApiDataInitializer {
 
     @Inject
-    private BookApiConsumer bookApiConsumer;
+    private ParserService parserService;
 
     @Inject
-    private BookMapper bookMapper;
-
-    @Inject
-    private AuthorMapper authorMapper;
-
-    @Inject
-    private BookDao bookdao;
-
-    @Inject
-    private AuthorDao authorDao;
-
-    @Inject
-    private KindDao kindDao;
-
-    @Inject
-    private KindMapper kindMapper;
-
-    @Inject
-    private GenreDao genreDao;
-
-    @Inject
-    private GenreMapper genreMapper;
-
-    @Inject
-    private EpochDao epochDao;
-
-    @Inject
-    private EpochMapper epochMapper;
+    private BookService bookService;
 
     private Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+    private int checkStatus(String address) {
+
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target(address);
+        try (Response response = webTarget.request().get()) {
+            if (Response.Status.OK.equals(response.getStatusInfo())) {
+                return response.getStatus();
+            }
+            int status = response.getStatus();
+            throw new IllegalStateException("Error: " + status);
+        }
+    }
 
     private static final String URI = "http://isa-proxy.blueazurit.com/books/books/";
     private static final String AUTHOR_URI = "http://isa-proxy.blueazurit.com/books/authors/";
@@ -63,70 +45,27 @@ public class ApiDataInitializer {
     private static final String EPOCH_URI = "http://isa-proxy.blueazurit.com/books/epochs/";
 
 
-    @PostConstruct
-    public void setApi() throws IOException {
+    public String getApiFromUrl() throws IOException {
 
-        List<AuthorJson> authorJsons = bookApiConsumer.consume(getAuthorClientTarget(), AuthorJson.class);
-        authorJsons.forEach(authorJson -> {
-            Author author = authorMapper.mapApiRequestToEntity(authorJson);
-            authorDao.addAuthor(author);
-        });
-
-        List<KindJson> kindJsons = bookApiConsumer.consume(getKindClientTarget(), KindJson.class);
-        kindJsons.forEach(kindJson -> {
-            LiteratureKind kind = kindMapper.mapApiRequestToEntity(kindJson);
-            kindDao.addKind(kind);
-        });
-
-        List<GenreJson> genreJsonList = bookApiConsumer.consume(getGenreClientTarget(), GenreJson.class);
-        genreJsonList.forEach(b -> {
-            Genre genre = genreMapper.mapApiRequestToEntity(b);
-            genreDao.addGenre(genre);
-        });
-
-        List<EpochJson> epochJsonList = bookApiConsumer.consume(getEpochClientTarget(), EpochJson.class);
-        epochJsonList.forEach(b -> {
-            Epoch epoch = epochMapper.mapApiRequestToEntity(b);
-            epochDao.addEpoch(epoch);
-        });
-
-        List<BookJson> bookJsonList = bookApiConsumer.consume(getBookClientTarget(), BookJson.class);
-        bookJsonList.forEach(b -> {
-            Book book = bookMapper.mapApiToEntity(b);
-            Author authorByName = authorDao.findAuthorByName(b.getAuthor());
-            LiteratureKind kindByName = kindDao.findKindByName(b.getKind());
-            Genre genreByName = genreDao.findGenreByName(b.getGenre());
-            Epoch epochByName = epochDao.findEpochByName(b.getEpoch());
-            book.setAuthor(authorByName);
-            book.setGenre(genreByName);
-            book.setKind(kindByName);
-            book.setEpoch(epochByName);
-            bookdao.addBook(book);
-        });
+        List<BookDTO> books = new ArrayList<>();
+        //Client client = ClientBuilder.newClient();
+        WebTarget webTarget = getBookClientTarget();
+        String strResponse = "";
+        try (Response response = webTarget.request().get()) {
+            if (Response.Status.OK.equals(response.getStatusInfo())) {
+                strResponse = response.readEntity(String.class);
+                return strResponse;
+            }
+            int status = response.getStatus();
+            throw new IllegalStateException("Error: " + status);
+        }
     }
 
-    private WebTarget getGenreClientTarget() {
-        Client client = ClientBuilder.newClient();
-        return client.target(GENRE_URI);
-    }
-
-    private WebTarget getEpochClientTarget() {
-        Client client = ClientBuilder.newClient();
-        return client.target(EPOCH_URI);
-    }
 
     private WebTarget getBookClientTarget() {
         Client client = ClientBuilder.newClient();
         return client.target(URI);
     }
 
-    private WebTarget getKindClientTarget() {
-        Client client = ClientBuilder.newClient();
-        return client.target(KIND_URI);
-    }
-
-    private WebTarget getAuthorClientTarget() {
-        Client client = ClientBuilder.newClient();
-        return client.target(AUTHOR_URI);
-    }
 }
+
---
+++ web/src/main/java/com/infoshareacademy/service/ApiStarter.java
@@ -0,0 +1,31 @@
+package com.infoshareacademy.service;
+
+import com.infoshareacademy.dto.BookDTO;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.List;
+
+@Startup
+@Singleton
+public class ApiStarter {
+
+    @Inject
+    ParserService parserService;
+
+    @Inject
+    ApiDataInitializer apiDataInitializer;
+
+    @Inject
+    BookService bookService;
+
+    @PostConstruct
+    private void setApi () throws IOException {
+        List<BookDTO> books = parserService.parse(apiDataInitializer.getApiFromUrl(), BookDTO.class);
+        bookService.addBooks(books);
+
+    }
+}
