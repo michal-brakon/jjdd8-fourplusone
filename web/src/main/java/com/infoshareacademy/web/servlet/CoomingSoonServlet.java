0f0a2339641a444f4dc27d3aab47212f76df572b
michal-brakon <zielonydeveloper@o2.pl>
2020-01-19 12:45:02 +0100

    fixes

---
+++ web/src/main/java/com/infoshareacademy/web/servlet/CoomingSoonServlet.java
@@ -0,0 +1,56 @@
+package com.infoshareacademy.web.servlet;
+
+import com.infoshareacademy.domain.view.BookView;
+import com.infoshareacademy.freemarker.TemplateProvider;
+import com.infoshareacademy.service.BookService;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet("/soon")
+public class CoomingSoonServlet extends HttpServlet {
+
+    @Inject
+    private BookService bookService;
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter writer = resp.getWriter();
+        List<BookView> bookViewList = bookService.booksForPagination();
+
+        Template template = templateProvider
+                .getTemplate(getServletContext(),
+                        "comingSoon.ftlh");
+        Map<String, Object> model = new HashMap<>();
+        model.put("soon", bookViewList);
+        try {
+            template.process(model, writer);
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
