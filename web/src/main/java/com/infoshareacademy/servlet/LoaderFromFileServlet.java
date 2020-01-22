bf0083a0d3f2cf26dc168dce873be81d4efb2533
lukaszwolek <lucas83122@gmail.com>
2020-01-22 22:48:41 +0100

    remove unused imports

--- web/src/main/java/com/infoshareacademy/servlet/LoaderFromFileServlet.java
+++ web/src/main/java/com/infoshareacademy/servlet/LoaderFromFileServlet.java
@@ -11,7 +11,6 @@ import freemarker.template.TemplateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
