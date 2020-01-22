ce00c966a7aa8b3ec07dd47a30e9d79e672b80f5
lukaszwolek <lucas83122@gmail.com>
2020-01-22 20:37:44 +0100

    add HealthCheck, add java13 support

--- web/src/main/java/com/infoshareacademy/api/HealthCheck.java
+++ web/src/main/java/com/infoshareacademy/api/HealthCheck.java
@@ -1,4 +1,26 @@
 package com.infoshareacademy.api;
 
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+import java.io.PrintWriter;
+
+@Stateless
 public class HealthCheck {
+
+    @GET
+    @Path("/api/health")
+    public int HealthCheck() {
+
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("/api/health");
+        Response response = webTarget.request().get();
+        System.out.println(response.getStatus());
+
+        return response.getStatus();
+    }
 }
