f12fe8ef76b0c24c70685770b9c4d0e04ecc80b1
michal-brakon <zielonydeveloper@o2.pl>
2020-01-29 15:17:56 +0100

    filter authotization

---
+++ web/src/main/java/com/infoshareacademy/filter/AuthorizationFilter.java
@@ -0,0 +1,47 @@
+package com.infoshareacademy.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter
+        (filterName = "AuthorizationFilter",
+                urlPatterns = {"/*"}
+        )
+public class AuthorizationFilter implements Filter {
+
+    private Logger logger = LoggerFactory.getLogger(AuthorizationFilter.class.getName());
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        String role = servletRequest.getParameter("role");
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        if (role == null || role.isEmpty()) {
+            ((HttpServletResponse) servletResponse).sendRedirect("/");
+        } else if (role.equals("admin")) {
+            ((HttpServletResponse) servletResponse).sendRedirect("/admin");
+        } else if (role.equals("user")) {
+            ((HttpServletResponse) servletResponse).sendRedirect("/user");
+        } else if (role.equals("superAdmin")) {
+            ((HttpServletResponse) servletResponse).sendRedirect("/admin");
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
