19304fee5f35ca5a4949e0ca26ebe4cf26aab899
Dariusz Czajkowski <czajk.dariusz@gmail.com>
2020-02-03 01:41:26 +0100

    api starter commented

--- web/src/main/java/com/infoshareacademy/service/Scheduler.java
+++ web/src/main/java/com/infoshareacademy/service/Scheduler.java
@@ -1,25 +1,25 @@
-package com.infoshareacademy.service;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.ejb.Schedule;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.inject.Inject;
-
-@Singleton
-@Startup
-public class Scheduler {
-
-    private static final Logger logger = LoggerFactory.getLogger(Scheduler.class.getName());
-
-    @Inject
-    ReservationService reservationService;
-
-    @Schedule(hour = "0", minute = "20/5", second = "0", info = "Every 1 minute timer")
-    public void Schedule () {
-        reservationService.removeUnconfirmedReservations();
-        logger.debug("Checking unconfirmed reservations");
-    }
-}
+//package com.infoshareacademy.service;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import javax.ejb.Schedule;
+//import javax.ejb.Singleton;
+//import javax.ejb.Startup;
+//import javax.inject.Inject;
+//
+//@Singleton
+//@Startup
+//public class Scheduler {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(Scheduler.class.getName());
+//
+//    @Inject
+//    ReservationService reservationService;
+//
+//    @Schedule(hour = "0", minute = "20/5", second = "0", info = "Every 1 minute timer")
+//    public void Schedule () {
+//        reservationService.removeUnconfirmedReservations();
+//        logger.debug("Checking unconfirmed reservations");
+//    }
+//}
